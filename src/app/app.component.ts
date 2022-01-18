d40cf2ddbf1fb35ed4589f52fc71d4973e8318af
Nguyen Van Nhuan <nhuannv@vnext.com.vn>
2022-01-18 19:55:52 +0700

    demo edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Directive } from '@angular/core';
 import { KeyboardService } from './keyboard.service';
 
 @Component({
@@ -8,26 +8,58 @@ import { KeyboardService } from './keyboard.service';
 })
 export class AppComponent {
   title = 'keyboard';
-
+  valueInput: string = "";
   constructor(private keyboardService: KeyboardService) {
+
+  }
+
+  getInputValue(event: any){
+    this.valueInput += event.target.value;
+    console.log(this.valueInput)
+    if(this.valueInput.length == 12){
+      //check recovery
+      console.log(this.valueInput)
+    }
+  }
+
+  checkInputCode(){
 
   }
 
   move(event: any, p: any, c: any, n: any) {
     const length = c.value.length;
     const maxLength = c.getAttribute('maxlength');
+    //Khi input di chuyển đi
     if (length == maxLength) {
       if (n != '') {
         n.focus();
       }
     }
-    if (event.key == 'Backspace') {
-      if (p != '') {
-        p.focus();
+    //Khi xóa
+    else if (length != maxLength){
+      if(p!= '' && length < 2 ){
+        if(length < maxLength && n == ''){
+          p.focus()
+        }
+        //Khi xóa từ giữa dòng 1 kí tự
+        else if (length < maxLength && p.value.length == maxLength && n.value.length == maxLength){
+          c.focus()
+        } 
+        //Khi xóa từ cuối dòng
+        else{
+          p.focus()
+        }
       }
+      this.valueInput == ""
+      console.log (this.valueInput)
     }
-    console.log();
   }
 
+  onBackspace() {
+    this.keyboardService.fireBackspacePressed();
+  }
+  onEnter() {
+    this.keyboardService.fireEnterPressed();
+  }
   getLengthInput() {}
 }
